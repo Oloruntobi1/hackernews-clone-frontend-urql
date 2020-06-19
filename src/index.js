8f3bcb053e2bf9119cfe83d4ea15f1453196b161
Askari <blgntobi@gmail.com>
2020-06-19 01:53:04 +0100

    configured the urql and set the Provider component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider, Client, defaultExchanges } from "urql"
+
+
+const client = new Client({
+  url: "http://localhost:4000",
+  exchanges: defaultExchanges
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
